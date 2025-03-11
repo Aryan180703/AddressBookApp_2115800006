8356b09b8ec4b58ebffc93dc8fd8f27b6aeea481
Aryan Upadhyay <aryanupadhyay1807@gmail.com>
2025-03-11 18:06:38 +0530

    [Aryan Upadhyay] Merge: UC1 into main

---
+++ RepositoryLayer/Context/AddressBookDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RepositoryLayer.Context
+{
+    public class AddressBookDbContext : DbContext
+    {
+        public AddressBookDbContext(DbContextOptions<AddressBookDbContext> options) : base(options) { }     
+    }
+}
